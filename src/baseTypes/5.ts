d8aa8a4fc50063df9c97c27501b304695aa8089a
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2023-12-17 17:28:15 +0200

    part 1 is done

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -2,6 +2,8 @@
   Як ви визначите змінну в TypeScript, яка може приймати рядок або число (union type)? 
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
+type literal = 'enable' | 'disable';
 
-let union;
+
+let union: string | number;
 let literal;
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,10 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+const isWeekend = (value: number): boolean => {
+  if (value === 6 || value === 7) {
+    return true;
+  }
+  return false;
+}
